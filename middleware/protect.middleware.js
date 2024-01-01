91d1f4a2d3d7c9fe4f622a3b99b4d93965dc7920
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-02 06:11:32 +0700

    feat: protect authentication token

---
+++ middleware/protect.middleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const authenticationToken = (req, res, next) => {
+     const token = req.headers["authorization"];
+
+     if (!token) {
+          console.log("Token Not Found")
+          return res.status(401).json({
+               message: "Unauthorized: Missing Token"
+          });
+     }
+
+     const splitToken = token.split(' ')[1];
+
+     try {
+          const resultToken = jwt.verify(splitToken, process.env.JWT_SECRET);
+
+          if (!resultToken.userId) {
+               return res.status(401).json({
+                    message: "No Authorized"
+               });
+          } else {
+               console.log("Token Verified Successfully");
+               next();
+          }
+
+     } catch (error) {
+          console.log('Internal server error', error)
+          return res.status(500).json({
+               message: "Internal server error during token verification"
+          });
+     }
+};
+
+module.exports = {
+     authenticationToken
+}
