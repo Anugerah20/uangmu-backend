97d6d1b68bd11716db3add49bc47868787b7d90c
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-04 15:43:34 +0700

    add: route get user by id

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // Import Function Controller
-const { registerNewUser, loginUser } = require("../controller/user.controller");
+const { authenticationToken } = require("../middleware/protect.middleware");
+const { registerNewUser, loginUser, showDataUser } = require("../controller/user.controller");
 
 // Router Register
 router.post('/register', registerNewUser);
 
 // Router Login
 router.post('/login', loginUser);
+
+// Router Get User
+router.get("/:id", authenticationToken, showDataUser);
 
 module.exports = router;
