0770f6ca5de1a75544e2573923b73f54f6e4c4c9
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-05 16:25:27 +0700

    perf: add route edit user and check-token

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Import Function Controller
 const { authenticationToken } = require("../middleware/protect.middleware");
-const { registerNewUser, loginUser, showDataUser } = require("../controller/user.controller");
+const { registerNewUser, loginUser, showDataUser, getEditDataUser, checkToken } = require("../controller/user.controller");
 
 // Router Register
 router.post('/register', registerNewUser);
@@ -13,5 +13,12 @@ router.post('/login', loginUser);
 
 // Router Get User
 router.get("/:id", authenticationToken, showDataUser);
+
+// Route Get Edit User By Id
+router.get("/edit-profil/:id", authenticationToken, getEditDataUser);
+
+// Route Check Token
+router.get("/check-token", authenticationToken, checkToken);
+
 
 module.exports = router;
