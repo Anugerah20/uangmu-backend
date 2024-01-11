07d7f98b8791559a7d9f76b8df87a38b9f7b7bcf
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-11 20:53:52 +0700

    update: import multer, body parser

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const userRoute = require("./routes/user.route")
-
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const upload = multer();
 const app = express();
 const port = process.env.PORT || 3000;
 
 // For Request body
 app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(
+     bodyParser.urlencoded({
+          extended: true
+     })
+);
 
 // Request form data
 app.use(express.urlencoded({ extended: true }));
