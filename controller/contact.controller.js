2252287278a50397fd02a300b3e9b65a1001278f
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-08-30 10:18:59 +0700

    feat: Improve email validation in contact controller

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -8,16 +8,10 @@ const sendContact = async (req, res) => {
 
      const userId = req.user.id;
 
-     // Regex untuk validasi email
-     let emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
-
      try {
           // Buat kondisi jika fullname, email, dan message tidak diisi
           if (fullname === '' || email === '' || message === '') {
                return res.status(400).json({ error: 'Please fill in the data' });
-               // Buat kondisi jika email tidak valid
-          } else if (!emailRegex.test(email)) {
-               return res.status(400).json({ error: 'Email is not valid' });
           }
 
           const contact = await prisma.contact.create({
