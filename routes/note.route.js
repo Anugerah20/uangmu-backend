c3a105986ac5d7294a82842755f8173e017947d3
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-09-20 22:27:44 +0700

    Refactor note.route.js to add route for displaying all notes by user ID

--- routes/note.route.js
+++ routes/note.route.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const { authenticationToken } = require('../middleware/protect.middleware');
 
-const { createNote, getNoteById, editNoteById, deleteNoteById } = require('../controller/note.controller');
+const { createNote, getNoteById, editNoteById, deleteNoteById, getAllNote } = require('../controller/note.controller');
 
 // Route Create Note
 router.post('/note', authenticationToken, createNote);
 
 // Route Show Note By ID
 router.get('/get-note/:userId', getNoteById);
+
+// Route show All Note
+router.get('/get-all-note/:userId', authenticationToken, getAllNote);
 
 // Route Edit Note By ID
 router.put('/edit-note/:userId', authenticationToken, editNoteById);
